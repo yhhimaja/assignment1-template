8202f9bdfe0a542775fb53e025f1f9046e290dc0
Mohamad Bourji <mohd.bourji@gmail.com>
2022-02-10 21:22:21 -0600

    Update Portfolio.py

--- app/src/Portfolio.py
+++ app/src/Portfolio.py
@@ -1,3 +1,4 @@
 #Create a portfolio class that has the following three attributes:
 #1. ID: unique numerical identifier of the porfoltio
 #2. Description: description of the porfoltio (e.g., Finance/Real Estate/Tech)
+#3. Stocks: List of stocks in the portfolio

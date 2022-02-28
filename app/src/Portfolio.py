14322436003e81310f22f9120db942dfccd455a0
yhhimaja <92965129+yhhimaja@users.noreply.github.com>
2022-02-28 00:21:19 -0600

    Update Portfolio.py

--- app/src/Portfolio.py
+++ app/src/Portfolio.py
@@ -2,3 +2,10 @@
 #1. ID: unique numerical identifier of the porfoltio
 #2. Description: description of the porfoltio (e.g., Finance/Real Estate/Tech)
 #3. Stocks: List of stocks in the portfolio
+
+
+class Portfolio():
+    def __init__(self, ID, Description, Stocks):
+        self.ID= ID
+        self.Description = Description
+        self.Stocks = Stocks

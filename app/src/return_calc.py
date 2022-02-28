7d16f73590959e918798a9a7664017a56fc9c3bf
yhhimaja <92965129+yhhimaja@users.noreply.github.com>
2022-02-28 00:22:32 -0600

    Update return_calc.py

--- app/src/return_calc.py
+++ app/src/return_calc.py
@@ -13,3 +13,33 @@ def avg_return(stocks):
         return 0.0  if the list is empty
     '''
     pass
+
+
+
+from Portfolio import Portfolios
+from Stock import stock
+from EmptyStockList import Emptystock
+
+def returncalculation(Portfolios):
+    try:
+      returncalculation={}
+      for Portfolio in Portfolios:
+          average = average_return(Portfolio.Stock)
+          if returncalculation.get(Portfolio.ID) is None:
+              returncalculation[Portfolio.ID] =average
+    except Exception as e:
+                  print("Exception occurred while calculating return by portfolio:"+str('e'))
+    finally:
+        return returncalculation
+
+
+def average_return(stock):
+    try:
+        if len(stock)==0:
+            raise Emptystock
+            Total_Return=0
+            for Stock in stock:
+                    ReturnTotal=Total_Return+stock.Stockrateofreturn
+                    return ReturnTotal/len(stock)
+    except Exception as e:
+        print('error while getting an avgreturn of stocks'+str("e"))

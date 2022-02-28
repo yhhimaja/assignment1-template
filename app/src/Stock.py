936df97afbe4089da9cd652a3720f40e2d8d4a78
yhhimaja <92965129+yhhimaja@users.noreply.github.com>
2022-02-28 00:21:53 -0600

    Update Stock.py

--- app/src/Stock.py
+++ app/src/Stock.py
@@ -1,3 +1,9 @@
 #Create a Stock class the has the following attributes:
 # 1. ticker: the stock unique identifier
 # 2. rate_of_return: The stock's rate of return on a given day
+
+
+class Stocks():
+    def __init__(self, ticker, Calculate_return):
+        self.ticker = ticker
+        self.rate_of_return =Calculate_return
